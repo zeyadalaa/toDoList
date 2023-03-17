c84342a7c3a31ce1483db496f385a8cb17a262f1
zeyadalaa <zeyad.alaaeldin1998@gmail.com>
2023-03-17 20:08:32 +0200

    retrieve data from database

--- controllers/taskController.js
+++ controllers/taskController.js
@@ -1,6 +1,7 @@
 
 const Task = require("../models/taskModel")
 
+// Create documents
 exports.createTask = (req,res) => {
     const task = new Task(req.body)
     console.log(req.body); 
@@ -9,4 +10,16 @@ exports.createTask = (req,res) => {
     }).catch((err) => {
         console.log(err)
     })
+}
+
+
+// Find all documents
+exports.findAllTasks = async (req,res) => {
+    try{
+        const todos =  await Task.find({})
+        res.render("index",{todos})
+    }catch (err){
+        console.error(err);
+        res.status(500).send("Error retrieving tasks");
+    }
 }
